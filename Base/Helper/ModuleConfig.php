7676fa61c8333061030d5b01a9dcea57d23c82e3
kom <r.komarianskyi@zinitsolutions.com>
2018-10-14 13:16:26 +0300

    base ext init

---
+++ Base/Helper/ModuleConfig.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Kom\Base\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManager;
+
+abstract class ModuleConfig extends AbstractHelper
+{
+    /**
+     * @var StoreManager
+     */
+    protected $storeManager;
+
+    /**
+     * ModuleConfig constructor.
+     * @param StoreManager $storeManager
+     * @param Context $context
+     */
+    public function __construct(
+        StoreManager $storeManager,
+        Context $context
+    ) {
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
+    }
+
+    /**
+     * @param $paramName
+     * @return mixed
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    protected function getConfigValue($paramName)
+    {
+        return $this->scopeConfig->getValue($this->getModuleScopePath() . '/' . $paramName,
+            ScopeInterface::SCOPE_STORES,
+            $this->storeManager->getStore()->getCode());
+    }
+
+    /**
+     * @return bool
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function isActive()
+    {
+        return boolval($this->getConfigValue('active'));
+    }
+
+    /**
+     * Should return module settings section ID.
+     *
+     * @return string
+     */
+    abstract protected function getModuleScopePath();
+
+}
